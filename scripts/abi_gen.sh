1619555d538eda2a19c27d785fde93cffef92f36
Robert Sun <sunwrobert@gmail.com>
2022-01-13 00:56:57 -0800

    add back bin/sh

--- scripts/abi_gen.sh
+++ scripts/abi_gen.sh
@@ -1,1 +1,3 @@
+#!/bin/sh
+
 yarn run abi-gen --abis 'src/wyvern-ethereum/build/contracts/@(WyvernToken|WyvernDAO|WyvernExchange|WyvernProxyRegistry|WyvernAtomicizer).json' --templates src/abi_templates --output src/abi_gen
